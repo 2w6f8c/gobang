f8c2039e33a7bfa95241f148058bbf7b7d896689
2w6f8c <2428391347@qq.com>
2019-05-20 20:11:12 +0800

    change randomPlay directory

--- randomPlay.h
+++ randomPlay.h
@@ -5,7 +5,7 @@
 #ifndef GOBANG_RANDOMPLAY_H
 #define GOBANG_RANDOMPLAY_H
 
-#include "../def.h"
+#include "def.h"
 #include <windows.h>
 
 POINT RandomPlay(int chessPoints[BOARD_CELL_NUM + 1][BOARD_CELL_NUM + 1]);
