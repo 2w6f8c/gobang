20fc37e7a5f5307584b572fb14cec5891cbf1843
2w6f8c <2428391347@qq.com>
2019-06-11 23:55:18 +0800

    update

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ c2019课设。
 - ~~优化（增加搜索深度）~~
 - ~~悔棋~~
 - ~~调整init.c中函数位置~~
+
+## build
+
+Simply click build.bat.
---
+++ build/CMakeFiles/gobang.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gobang.dir/main.c.obj"
+  "CMakeFiles/gobang.dir/init.c.obj"
+  "CMakeFiles/gobang.dir/draw.c.obj"
+  "CMakeFiles/gobang.dir/engine.c.obj"
+  "CMakeFiles/gobang.dir/randomPlay.c.obj"
+  "CMakeFiles/gobang.dir/evaluate.c.obj"
+  "CMakeFiles/gobang.dir/alphaBeta.c.obj"
+  "CMakeFiles/gobang.dir/stack.c.obj"
+  "gobang.pdb"
+  "gobang.exe"
+  "gobang.exe.manifest"
+  "libgobang.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gobang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
